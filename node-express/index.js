da2ea0e722287c8f1dc21bdc4838cdbb370bb050
bhagya2002 <bhagya02.p@gmail.com>
2022-05-11 12:54:45 -0600

    node, expressJS

---
+++ node-express/index.js
@@ -0,0 +1,29 @@
+// import express
+const express = require("express");
+const http = require("http");
+const morgan = require("morgan");
+
+// init localhost info
+const hostname = "localhost";
+const port = 3000;
+
+// instantiate express server
+const app = express();
+app.use(morgan("dev")); // start use of morgan
+
+// if the URL with filename path exists then go to it, else run the next app.use()
+app.use(express.static(__dirname + "/public")); // start use of express static, __dirname is the current directory
+
+// when server runs
+app.use((req, res, next) => {
+  //   if set up correctly
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.end("<html><head><h1>This is an Express server</h1></head></html>");
+});
+
+// start server
+const server = http.createServer(app);
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
